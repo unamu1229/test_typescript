892d3ee825ed478a345859510f404e025c14785d
hiroshi-yoneda <hiroshi-yoneda@dip-net.co.jp>
2024-12-17 18:45:03 +0900

    async await

---
+++ hoge.ts
@@ -0,0 +1,11 @@
+export type StepQuestion = {
+    working_type_ans?: "hgoehgeo";
+}
+
+export type DynamicFormSection = keyof StepQuestion;
+
+const hoge: DynamicFormSection[] = [];
+
+hoge.push('working_type_ans');
+
+console.log(hoge);
