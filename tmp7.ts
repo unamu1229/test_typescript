cd46c943a2440e56a284d006848235095502f7e0
hiroshi-yoneda <hiroshi-yoneda@dip-net.co.jp>
2025-01-17 20:06:15 +0900

    foo

---
+++ tmp7.ts
@@ -0,0 +1,23 @@
+export type CommonRegistration = {
+    Id?: string;
+}
+
+export type MailMagazineCDRecord = CommonRegistration & {
+    Email__c?: string;
+};
+
+export type MailRegistrationCDRecord = CommonRegistration & {
+    Time_Career_Change_Memo__c?: string;
+}
+
+export type CandidateRecord = MailMagazineCDRecord | MailRegistrationCDRecord;
+
+const candidateRecord: CandidateRecord = {
+    Id: "1",
+    Email__c: "hoge",
+    Time_Career_Change_Memo__c: "fuga"
+}
+
+console.log(candidateRecord);
+
+
